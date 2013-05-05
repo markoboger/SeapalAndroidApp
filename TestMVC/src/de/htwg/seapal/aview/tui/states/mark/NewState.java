30c35fad52ce523fb38de1765747e693c61c7d5b
chbraun <chbraun@htwg-konstanz.de>
2013-05-05 12:20:24 +0200

    fix routeMark Problem

--- TestMVC/src/de/htwg/seapal/aview/tui/states/mark/NewState.java
+++ TestMVC/src/de/htwg/seapal/aview/tui/states/mark/NewState.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import de.htwg.seapal.aview.tui.StateContext;
 import de.htwg.seapal.aview.tui.TuiState;
 import de.htwg.seapal.aview.tui.activity.MarkActivity;
+import de.htwg.seapal.controller.IMarkController;
 
 public class NewState implements TuiState {
 
@@ -15,12 +16,14 @@ public class NewState implements TuiState {
 
 	@Override
 	public boolean process(StateContext context, String input) {
+		IMarkController controller = ((MarkActivity) context).getController();
 		if (input.equals("q")) {
 			context.setState(new StartState());
 			return false;
 		}
-		UUID mark = ((MarkActivity) context).getController().newMark();
-		((MarkActivity) context).getController().setName(mark, input);
+		UUID mark = controller.newMark();
+		controller.setName(mark, input);
+		controller.setIsRouteMark(mark, false);
 		context.setState(new ShowState(mark));
 		return true;
 	}
