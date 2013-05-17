504a25c83964040a7e35f12c1d300e25eacb3249
chbraun <chbraun@htwg-konstanz.de>
2013-05-18 01:11:44 +0200

    create first layout for boat, create packages, create boatactivity, fragment

---
+++ SeapalAndroidApp/src/de/htwg/seapal/aview/gui/activity/BoatActivity.java
@@ -0,0 +1,23 @@
+package de.htwg.seapal.aview.gui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import de.htwg.seapal.utils.observer.Event;
+import de.htwg.seapal.utils.observer.IObserver;
+
+public class BoatActivity extends Activity implements IObserver {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	
+	
+	@Override
+	public void update(Event event) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ SeapalAndroidApp/src/de/htwg/seapal/aview/gui/fragment/BoatListFragment.java
@@ -0,0 +1,16 @@
+package de.htwg.seapal.aview.gui.fragment;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+public class BoatListFragment extends ListFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	
+	
+}
