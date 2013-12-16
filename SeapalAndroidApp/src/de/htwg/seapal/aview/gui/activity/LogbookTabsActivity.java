5c02ab7aa01032d7adb50a0f23166c4df9128701
jakub Werner <jack@jaxxnet.de>
2013-12-16 21:57:04 +0100

    new Menues for Crew and account and fixed bug that boatlist is above basedrawer

--- SeapalAndroidApp/src/de/htwg/seapal/aview/gui/activity/LogbookTabsActivity.java
+++ SeapalAndroidApp/src/de/htwg/seapal/aview/gui/activity/LogbookTabsActivity.java
@@ -1,7 +1,6 @@
 package de.htwg.seapal.aview.gui.activity;
 
 import android.app.ActionBar;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,28 +10,30 @@ import com.google.inject.Inject;
 import java.util.UUID;
 
 import de.htwg.seapal.R;
+import de.htwg.seapal.aview.gui.fragment.AccountFragment;
 import de.htwg.seapal.aview.gui.fragment.BoatListFragment;
 import de.htwg.seapal.aview.gui.fragment.CrewFragment;
 import de.htwg.seapal.aview.gui.fragment.LogbookFragment;
 import de.htwg.seapal.aview.gui.fragment.LogbookSlideFragment;
+import de.htwg.seapal.aview.listener.OnCreateOptionsMenuListener;
+import de.htwg.seapal.aview.listener.TabListener;
 
 /**
  * Created by jakub on 12/10/13.
  */
-public class LogbookTabsActivity extends BaseDrawerActivity implements ActionBar.TabListener, BoatListFragment.OnBoatNameSelectedListener{
+public class LogbookTabsActivity extends BaseDrawerActivity implements BoatListFragment.OnBoatNameSelectedListener {
 
 
     public static final int LOGBOOK_FRAGMENT = 1;
     public static final int CREW_FRAGMENT = 0;
-
     private int mPosition;
     @Inject
-    private  LogbookFragment mLogbookFragment;
+    private LogbookFragment mLogbookFragment;
     private boolean mLogbookFragmentCreated = false;
     private boolean mCrewFragmentCreated = false;
-
     @Inject
     private CrewFragment mCrewFragment;
+    private Menu mMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +43,36 @@ public class LogbookTabsActivity extends BaseDrawerActivity implements ActionBar
 
         ActionBar ab = getActionBar();
 
-        ab.addTab(ab.newTab().setText("Crew").setTabListener(this));
-        ab.addTab(ab.newTab().setText("Logbook").setTabListener(this));
+        ab.addTab(ab.newTab().setText("Account").setTabListener(new TabListener<AccountFragment>(R.id.logbook_fragment_tabs, "Account", AccountFragment.class, this, new OnCreateOptionsMenuListener() {
+            @Override
+            public void onCreateMenu() {
+                if (mMenu != null)
+                    mMenu.clear();
+
+            }
+        })));
+        ab.addTab(ab.newTab().setText("Crew").setTabListener(new TabListener<CrewFragment>(R.id.logbook_fragment_tabs, "crew", CrewFragment.class, this, new OnCreateOptionsMenuListener() {
+            @Override
+            public void onCreateMenu() {
+                if (mMenu != null) {
+                    mMenu.clear();
+                    getMenuInflater().inflate(R.menu.logbook_crew_menu, mMenu);
+
+                }
+
+
+            }
+        })));
+        ab.addTab(ab.newTab().setText("Logbook").setTabListener(new TabListener<LogbookFragment>(R.id.logbook_fragment_tabs, "logbook", LogbookFragment.class, this, new OnCreateOptionsMenuListener() {
+            @Override
+            public void onCreateMenu() {
+                if (mMenu != null) {
+                    mMenu.clear();
+                    getMenuInflater().inflate(R.menu.logbook_boat_menu, mMenu);
+
+                }
+            }
+        })));
         ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
 
@@ -52,67 +81,17 @@ public class LogbookTabsActivity extends BaseDrawerActivity implements ActionBar
     @Override
     public void onBoatSelected(int position, UUID uuid) {
         LogbookSlideFragment logbookSlideFragment = (LogbookSlideFragment) getSupportFragmentManager().findFragmentById(R.id.logbook_slide_fragment);
-        if(logbookSlideFragment != null) {
+        if (logbookSlideFragment != null) {
             logbookSlideFragment.updateBoatView(position, uuid);
         }
 
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.logbook_menu, menu);
+        mMenu = menu;
         return true;
-    }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-        switch (tab.getPosition()) {
-            case CREW_FRAGMENT:
-                ft.remove(mCrewFragment);
-                ft.replace(R.id.logbook_fragment_tabs, mCrewFragment);
-                mPosition = CREW_FRAGMENT;
-                break;
-            case LOGBOOK_FRAGMENT:
-                ft.remove(mLogbookFragment);
-                ft.replace(R.id.logbook_fragment_tabs, mLogbookFragment);
-                mPosition = LOGBOOK_FRAGMENT;
-
-                break;
-
-        }
-
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-        switch (tab.getPosition()) {
-            case CREW_FRAGMENT:
-                ft.hide(mCrewFragment);
-                break;
-            case LOGBOOK_FRAGMENT:
-                ft.hide(mLogbookFragment);
-                break;
-
-        }
-
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-        switch (tab.getPosition()) {
-            case CREW_FRAGMENT:
-                ft.show(mCrewFragment);
-                mPosition = CREW_FRAGMENT;
-                break;
-            case LOGBOOK_FRAGMENT:
-                ft.show(mLogbookFragment);
-                mPosition = LOGBOOK_FRAGMENT;
-                break;
-
-        }
-
     }
 
     @Override
@@ -120,19 +99,19 @@ public class LogbookTabsActivity extends BaseDrawerActivity implements ActionBar
         switch (item.getItemId()) {
             case R.id.logbookmenu_new:
                 if (mPosition == LOGBOOK_FRAGMENT) {
-                    LogbookSlideFragment logbookSlideFragment =  (LogbookSlideFragment) getSupportFragmentManager().findFragmentById(R.id.logbook_slide_fragment);
+                    LogbookSlideFragment logbookSlideFragment = (LogbookSlideFragment) getSupportFragmentManager().findFragmentById(R.id.logbook_slide_fragment);
                     logbookSlideFragment.onNewBoat();
                 }
                 break;
             case R.id.logbookmenu_delete:
                 if (mPosition == LOGBOOK_FRAGMENT) {
-                    LogbookSlideFragment logbookSlideFragment =  (LogbookSlideFragment) getSupportFragmentManager().findFragmentById(R.id.logbook_slide_fragment);
+                    LogbookSlideFragment logbookSlideFragment = (LogbookSlideFragment) getSupportFragmentManager().findFragmentById(R.id.logbook_slide_fragment);
                     logbookSlideFragment.onDeleteBoat();
                 }
                 break;
             case R.id.logbookmenu_save:
                 if (mPosition == LOGBOOK_FRAGMENT) {
-                    LogbookSlideFragment logbookSlideFragment =  (LogbookSlideFragment) getSupportFragmentManager().findFragmentById(R.id.logbook_slide_fragment);
+                    LogbookSlideFragment logbookSlideFragment = (LogbookSlideFragment) getSupportFragmentManager().findFragmentById(R.id.logbook_slide_fragment);
                     logbookSlideFragment.onSaveBoat();
                 }
                 break;
---
+++ SeapalAndroidApp/src/de/htwg/seapal/aview/gui/fragment/AccountFragment.java
@@ -0,0 +1,9 @@
+package de.htwg.seapal.aview.gui.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by jakub on 12/16/13.
+ */
+public class AccountFragment extends Fragment{
+}
